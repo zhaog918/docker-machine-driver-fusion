547b5a2cf47575decd27636f3a99c8af590e95df
Gang Zhaog <zhaog918@gmail.com>
2019-08-11 16:50:11 +0800

    reclean vendor

--- vendor/github.com/zhaog918/docker-machine-driver-fusion/drivers/fusion/fusion.go
+++ vendor/github.com/zhaog918/docker-machine-driver-fusion/drivers/fusion/fusion.go
@@ -5,5 +5,5 @@ package fusion
 import "github.com/docker/machine/libmachine/drivers"
 
 func NewDriver(hostName, storePath string) drivers.Driver {
-	return drivers.NewDriverNotSupported("vmwarefusion", hostName, storePath)
+	return drivers.NewDriverNotSupported("fusion", hostName, storePath)
 }
